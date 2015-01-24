cbc944a8bd580e8ad29f08850ece21ecc6f57172
bartlomiej-gora <bartlomiej.gora@gmail.com>
2015-01-24 20:11:33 +0100

    Fixed exception tests.

--- src/test/java/pl/bgora/CalculatorTest.java
+++ src/test/java/pl/bgora/CalculatorTest.java
@@ -10,9 +10,8 @@ import org.junit.Test;
 import pl.bgora.rpn.Calculator;
 import pl.bgora.rpn.exceptions.RPNException;
 
-
 public class CalculatorTest {
-	
+
 	private Calculator calc;
 
 	@Before
@@ -21,91 +20,56 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void testCalculate() {
-		BigDecimal result = null;
-		try {
-			result = calc.calculate("2^3*(12/6)+18/3+5.0/2");	
-		} catch (RPNException e) {
-			e.printStackTrace();
-		}
+	public void testCalculate() throws RPNException {
+		BigDecimal result = calc.calculate("2^3*(12/6)+18/3+5.0/2");
 		assertEquals("2^3*(12/6)+18/3+5.0/2", BigDecimal.valueOf(24.5), result);
 	}
-	
-	@Test
-	public void testMultiply(){
-		BigDecimal result = null;
-		try {
-			result = calc.calculate("2*8");
-		} catch (RPNException e) {
-			e.printStackTrace();
-		}
-		assertEquals("2*8",BigDecimal.valueOf(16), result);
-	}
-	
-	@Test
-	public void testPower(){
-		BigDecimal result = null;
-		try {
-			result = calc.calculate("2^8");
-		} catch (RPNException e) {
-			e.printStackTrace();
-		}
-		assertEquals("2^8", BigDecimal.valueOf(256), result);
-	}
-	
-	@Test
-	public void testAdd(){
-		BigDecimal result = null;
-		try {
-			result = calc.calculate("2+8");
-		} catch (RPNException e) {
-			e.printStackTrace();
-		}
-		assertEquals("2+8", BigDecimal.valueOf(10),  result);
-	}
-	
-	@Test
-	public void testSub(){
-		BigDecimal result = null;
-		try {
-			result = calc.calculate("2-5");
-		} catch (RPNException e) {
-			e.printStackTrace();
-		}
-		assertEquals("2-5", BigDecimal.valueOf(-3), result);
-	}
-	
-	@Test
-	public void testDiv(){
-		BigDecimal result = null;
-		try {
-			result = calc.calculate("10.0/4");
-		} catch (RPNException e) {
-			e.printStackTrace();
-		}
-		assertEquals("10.0/4",BigDecimal.valueOf(2.5), result);
-	}
-	
-	@Test
-	public void testSinus(){
-		BigDecimal result = null;
-		try {
-			result = calc.calculate("sin(2)");
-		} catch (RPNException e) {
-			e.printStackTrace();
-		}
-		assertEquals(BigDecimal.valueOf(Math.sin(2)), result);
-	}
-	
 
 	@Test
-	public void testSinusPlus(){
-		BigDecimal result = null;
-		try {
-			result = calc.calculate("sin(1+1)");
-		} catch (RPNException e) {
-			e.printStackTrace();
-		}
-		assertEquals(BigDecimal.valueOf(Math.sin(1+1)), result);
+	public void testMultiply() throws RPNException {
+		BigDecimal result = calc.calculate("2*8");
+		assertEquals("2*8", BigDecimal.valueOf(16), result);
+	}
+
+	@Test
+	public void testPower() throws RPNException {
+		BigDecimal result = calc.calculate("2^8");
+		assertEquals("2^8", BigDecimal.valueOf(256), result);
+	}
+
+	@Test
+	public void testAdd() throws RPNException {
+		BigDecimal result = calc.calculate("2+8");
+		assertEquals("2+8", BigDecimal.valueOf(10), result);
+	}
+
+	@Test
+	public void testSub() throws RPNException {
+		BigDecimal result  = calc.calculate("2-5");
+		assertEquals("2-5", BigDecimal.valueOf(-3), result);
+	}
+
+	@Test
+	public void testDiv() throws RPNException {
+		BigDecimal result = calc.calculate("10.0/4");
+		assertEquals("10.0/4", BigDecimal.valueOf(2.5), result);
+	}
+
+	@Test
+	public void testSinus() throws RPNException {
+		BigDecimal result = calc.calculate("sin(2)");
+		assertEquals(BigDecimal.valueOf(Math.sin(2)), result);
+	}
+
+	@Test
+	public void testSinusPlus() throws RPNException {
+		BigDecimal result  = calc.calculate("sin(1+1)");
+		assertEquals(BigDecimal.valueOf(Math.sin(1 + 1)), result);
+	}
+	
+	@Test(expected=RPNException.class)
+	public void shouldThrowRPNException() throws RPNException
+	{
+		calc.calculate("aaaaa");
 	}
 }
