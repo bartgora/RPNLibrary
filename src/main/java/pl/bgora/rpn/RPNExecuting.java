6bc826b51575daad667ff9dcfefa31cf5f5ed3e1
bartlomiej-gora <bartlomiej.gora@gmail.com>
2017-06-13 16:50:13 +0200

    #11: licence

--- src/main/java/pl/bgora/rpn/RPNExecuting.java
+++ src/main/java/pl/bgora/rpn/RPNExecuting.java
@@ -1,3 +1,22 @@
+/*
+ * RPNCalculator - Reverse Polish Notation mathematics Library
+ * Copyright (C) 2011  Bartłomiej Góra
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Contact: bartlomiej.gora@gmail.com
+ */
+
 package pl.bgora.rpn;
 
 import pl.bgora.rpn.exceptions.NoSuchFunctionFound;
@@ -36,5 +55,5 @@ public interface RPNExecuting {
      * @return Calculation result as BigDecimal.
      * @throws NoSuchFunctionFound thrown if Executing object cannot find method.
      */
-     BigDecimal executeFunction(String functionName, RoundingMode mode, String... arguments) throws NoSuchFunctionFound;
+    BigDecimal executeFunction(String functionName, RoundingMode mode, String... arguments) throws NoSuchFunctionFound;
 }
