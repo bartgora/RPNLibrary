675518fde019600f8e6ae75caf05818beb1e587e
bgora <bartlomiej.gora@infobip.com>
2020-10-30 15:25:52 +0100

    issue-82: Changed DivideOperatorStrategy to use BigDecimals

--- src/main/java/com/github/bgora/rpnlibrary/operators/DivideOperatorStrategy.java
+++ src/main/java/com/github/bgora/rpnlibrary/operators/DivideOperatorStrategy.java
@@ -24,17 +24,14 @@ import java.math.MathContext;
 
 public class DivideOperatorStrategy extends AbstractOperatorStrategy {
 
-
     public DivideOperatorStrategy() {
         super("/", 2);
     }
 
     @Override
     public BigDecimal execute(String first, String second, final MathContext mathContext) {
-        Double big1 = new Double(first);
-        Double big2 = new Double(second);
-        return BigDecimal.valueOf(big1 / big2);
+        BigDecimal big1 = new BigDecimal(first, mathContext);
+        BigDecimal big2 = new BigDecimal(second, mathContext);
+        return big1.divide(big2, 4,  mathContext.getRoundingMode());
     }
-
-
 }
---
+++ src/test/java/com/github/bgora/rpnlibrary/operators/DivideOperatorStrategyTest.java
@@ -0,0 +1,66 @@
+package com.github.bgora.rpnlibrary.operators;
+
+import org.assertj.core.data.Percentage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+class DivideOperatorStrategyTest {
+
+    private DivideOperatorStrategy tested;
+    private MathContext mathContext;
+
+    @BeforeEach
+    void setup() {
+        tested = new DivideOperatorStrategy();
+        mathContext = MathContext.DECIMAL64;
+    }
+
+    @Test
+    void executeShouldReturn4() {
+
+        final String givenFirstNumber = "8";
+        final String givenSecondNumber = "2";
+
+        final BigDecimal actual = tested.execute(givenFirstNumber, givenSecondNumber, mathContext);
+
+        then(actual).isCloseTo(BigDecimal.valueOf(4), Percentage.withPercentage(0));
+    }
+
+    @Test
+    void executeShouldReturnMinus4ForMinus2() {
+
+        final String givenFirstNumber = "8";
+        final String givenSecondNumber = "-2";
+
+        final BigDecimal actual = tested.execute(givenFirstNumber, givenSecondNumber, mathContext);
+
+        then(actual).isCloseTo(BigDecimal.valueOf(-4), Percentage.withPercentage(0));
+    }
+
+    @Test
+    void executeShouldReturn4ForMinus() {
+
+        final String givenFirstNumber = "-8";
+        final String givenSecondNumber = "-2";
+
+        final BigDecimal actual = tested.execute(givenFirstNumber, givenSecondNumber, mathContext);
+
+        then(actual).isCloseTo(BigDecimal.valueOf(4), Percentage.withPercentage(0));
+    }
+
+    @Test
+    void executeShouldReturn025() {
+
+        final String givenFirstNumber = "2";
+        final String givenSecondNumber = "8";
+
+        final BigDecimal actual = tested.execute(givenFirstNumber, givenSecondNumber, mathContext);
+
+        then(actual).isCloseTo(BigDecimal.valueOf(0.25), Percentage.withPercentage(0));
+    }
+}
