5690f3958919f5f0dec6159ef59586af4ce28e2a
bartlomiej-gora <bartlomiej.gora@gmail.com>
2023-07-02 14:02:04 +0200

    Refactoring to use chain of responsibility pattern

--- src/main/java/com/github/bgora/rpnlibrary/InputTransformer.java
+++ src/main/java/com/github/bgora/rpnlibrary/InputTransformer.java
@@ -19,13 +19,16 @@ import java.util.function.UnaryOperator;
  */
 public class InputTransformer implements UnaryOperator<String> {
 
-    public static final String EMPTY_SPACE = " ";
     protected final RPNChecking checker;
     private AbstractInputTransformerChain transformerChain;
 
     public InputTransformer(final RPNChecking checker) {
         this.checker = checker;
-        transformerChain = new DigitHandler(new DigitOrSeparatorHandler((new OperatorHandler(new WhitespaceHandler((new LetterHandler(null))), checker))));
+        transformerChain = new DigitOrSeparatorHandler(
+                new DigitHandler(
+                        new OperatorHandler(
+                                new WhitespaceHandler(
+                                        new LetterHandler(null)), checker)));
     }
 
     /**
@@ -40,71 +43,16 @@ public class InputTransformer implements UnaryOperator<String> {
         String result = "";
         final String inputValue = input.trim();
         int length = inputValue.length();
-        char character;
-
-        boolean lastWasDigit = false;
-        boolean lastWasOperator = false;
-        boolean lastWasWhiteSpace = false;
-        boolean lastWasLetter = false;
         TransformContext context = new TransformContext();
-
+        char character;
 
         for (int i = 0; i < length; i++) {
             character = inputValue.charAt(i);
             result = transformerChain.transformContext(context, character).getResult();
-//            if(result == null){
-//                throw new WrongArgumentException("Element \"" + character + "\" is not recognized by the Checker");
-//            }
-//            if (isDigitOrSeparator(character) && (lastWasDigit || !lastWasOperator)) {
-//                lastWasDigit = true;
-//                lastWasWhiteSpace = false;
-//                lastWasLetter = false;
-//                result.append(character);
-//            } else if (Character.isDigit(character)) {
-//                lastWasDigit = true;
-//                lastWasLetter = false;
-//                lastWasOperator = false;
-//                if (!lastWasWhiteSpace) {
-//                    result.append(EMPTY_SPACE);
-//                }
-//                result.append(character);
-//                lastWasWhiteSpace = false;
-//            } else if (checker.isOperatorOrBracket(String.valueOf(character))) {
-//                lastWasDigit = false;
-//                lastWasLetter = false;
-//                lastWasOperator = true;
-//                if (!lastWasWhiteSpace) {
-//                    result.append(EMPTY_SPACE);
-//                }
-//                result.append(character);
-//                lastWasWhiteSpace = false;
-//            } else if (Character.isWhitespace(character)) {
-//                if (!lastWasWhiteSpace && !lastWasDigit) {
-//                    result.append(EMPTY_SPACE);
-//                    lastWasWhiteSpace = true;
-//                }
-//                lastWasDigit = false;
-//                lastWasOperator = false;
-//            } else if (Character.isLetter(character)) {
-//                lastWasDigit = false;
-//                lastWasOperator = false;
-//                if (!lastWasLetter && !lastWasWhiteSpace) {
-//                    result.append(EMPTY_SPACE).append(character);
-//                } else {
-//                    result.append(character);
-//                }
-//                lastWasWhiteSpace = false;
-//                lastWasLetter = true;
-//            } else {
-//                throw new WrongArgumentException("Element \"" + character + "\" is not recognized by the Checker");
-//            }
         }
-
         return result.trim();
+
     }
 
-    private boolean isDigitOrSeparator(char c) {
-        return Character.isDigit(c) || c == '.' || c == ',';
-    }
 
 }
